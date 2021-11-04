ba198c31a6e50c6b89106d0d29cf3c6c2f810c17
Carlos Jiménez <charly.design.c@gmail.com>
2021-11-04 12:45:46 +0100

    main menu included

--- js/gameLauncher.js
+++ js/gameLauncher.js
@@ -1,16 +1,22 @@
-window.onload = function() {
+/* window.onload = function() {
     basicGame.init();
     
-};
+}; */
 
-/* window.onload = function() {
+window.onload = function() {
     document.getElementById("launch-btn").onclick = function() {
         document.querySelector('#launcher-menu').setAttribute('class', 'hide')
         document.querySelector('#intro').removeAttribute('class', 'hide')
         document.querySelector('#intro-video').play()
         setTimeout(function(){document.querySelector('#intro').setAttribute('class', 'hide');}, 29000)
-        basicGame.init()
+        setTimeout(function(){document.querySelector('#main-menu').removeAttribute('class', 'hide');}, 29000)
+        
         
     }
-} */
+
+    document.getElementById("play-btn").onclick = function() {
+        document.querySelector('#main-menu').setAttribute('class', 'hide')
+        basicGame.init()
+    }
+}
 
